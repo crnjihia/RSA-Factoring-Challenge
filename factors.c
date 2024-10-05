47de7b9c85d7487201061bea5615db99f829a0b7
Christopher <crnjihia@gmail.com>
2024-10-05 20:30:15 +0300

    Third Commit

--- factors.c
+++ factors.c
@@ -1,37 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Function to factorize a number into two factors
-void factorize(unsigned long long num) {
-    unsigned long long i;
-    for (i = 2; i <= num / 2; i++) {
-        if (num % i == 0) {
-            printf("%llu=%llu*%llu\n", num, i, num / i);
-            return;
+int main()
+{
+    long long int num = 239809320265259;
+    long int factor1 = 2;
+    long int factor2;
+
+    while (num % factor1)
+    {
+        if (factor1 <= num)
+        {
+            factor1++;
+        }
+        else {
+            return (-1);
         }
     }
-}
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
-
-    // Open the input file
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        printf("Error: Could not open file %s\n", argv[1]);
-        return 1;
-    }
-
-    unsigned long long num;
-    
-    // Read each number from the file and factorize it
-    while (fscanf(file, "%llu", &num) != EOF) {
-        factorize(num);
-    }
-
-    fclose(file);
-    return 0;
+    factor2 = num / factor1;
+    printf("%lld = %ld * %ld\n", num, factor2, factor1);
+    return (0);
 }
